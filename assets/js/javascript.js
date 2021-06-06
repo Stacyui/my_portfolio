cecf447a252a4ad1a48cac2315a4f38a646f5565
Stacyui <yuisong710@gmail.com>
2021-06-06 13:21:15 +0900

    Initial Commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.about-main-page-right').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true
+  });
+
+});
